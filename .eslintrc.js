2b30d5c1f50e4d5ee9dd5f9b3b2108a417906a40
chenzhenfei <605119298@qq.com>
2022-04-21 15:23:49 +0800

    feat: 初始化项目

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:vue/essential",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    parser: "@typescript-eslint/parser",
+    sourceType: "module",
+  },
+  plugins: ["vue", "@typescript-eslint"],
+  rules: {},
+};
---
+++ commitlint.config.js
@@ -0,0 +1,6 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  // 利用cz自定义提交规范
+  extends: ["cz"],
+  rules: {},
+};
